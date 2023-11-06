6a80a6330aff3c28dc0a96e2c477949af857d803
lewiszlw <lewiszlw520@gmail.com>
2023-11-06 16:53:48 +0800

    Run tests when enabled geozero feature

--- src/algorithm/geo/intersects.rs
+++ src/algorithm/geo/intersects.rs
@@ -334,6 +334,7 @@ mod tests {
     use geo::{point, polygon};
 
     #[test]
+    #[cfg_attr(not(feature = "geozero"), ignore)]
     fn wkbarray_intersects_arrays() {
         let polygon_array: PolygonArray<i32> = vec![
             polygon![
@@ -361,6 +362,7 @@ mod tests {
     }
 
     #[test]
+    #[cfg_attr(not(feature = "geozero"), ignore)]
     fn wkbarray_intersects_scalars() {
         let polygon_array: PolygonArray<i32> = vec![
             polygon![
